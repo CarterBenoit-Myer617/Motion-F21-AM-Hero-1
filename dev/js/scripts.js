b59e0f199306f76d60cd03d3b8ac4954330d7095
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2021-10-17 10:53:41 -0400

    up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,14 @@
 import { gsap } from "gsap";
+
+const mainTL = gsap.timeline();
+
+// name of timeline | do you want to start at the end or the beiginning of the timeline? | what do you want to anomate? \{how long os the animation? \what do you want to do?}
+
+// mainTL.to("#hero",{duration:2, alpha:0});
+
+mainTL.from("#hero",{duration:2, alpha:0});
+
+
+
+
+
