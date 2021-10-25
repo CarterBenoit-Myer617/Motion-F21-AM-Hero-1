450c25e0009301c9bb8a06da6c916d4c365ca8e0
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2021-10-25 05:29:06 -0400

    up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,77 @@
-import {gsap} from "gsap";
+import {
+    gsap
+} from "gsap";
 
 const mainTL = gsap.timeline();
 
-const heroHeight = document.querySelector("#hero");
+// mainTL.to("#Vector", {
+//     duation: 1,
+//     alpha: 0,
+//     scale: 2
+// });
 
-console.log(heroHeight.clientHeight + "px is the hight of the hero section");
-console.log(heroHeight.clientWidth + "px is the with of the hero section");
+function flowerstopmotion() {
 
-// name of timeline | do you want to start at the end or the beiginning of the timeline? | what do you want to anomate? \{how long os the animation? \what do you want to do?}
+    const tl = gsap.timeline
 
-// mainTL.to("#hero",{duration:2, alpha:0});
+    tl.from("#MacBook Pro - 1", {
+        duration: 2,
+    });
+    return tl;
 
-mainTL.from("#hero >div", {
-    duration: 1,
-    alpha: 0,
-    y: "-heroHight.clientHight",
-    scale: 3
-});
-mainTL.from("#hero h1", {
-    duration: 0.9,
-    alpha: 0,
-    y: "+=200"
-},"-=50%");
+}
+
+function outline() {
+
+    const tl = gsap.timeline
+
+    tl.from("#MacBook Pro - 2", {
+        duration: 4,
+    });
+    return tl;
+
+}
+
+mainTl.add(flowerstopmotion()).add(outline)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mainTL = gsap.timeline();
+
+// const heroHeight = document.querySelector("#hero");
+
+// console.log(heroHeight.clientHeight + "px is the hight of the hero section");
+// console.log(heroHeight.clientWidth + "px is the with of the hero section");
+
+// // name of timeline | do you want to start at the end or the beiginning of the timeline? | what do you want to anomate? \{how long os the animation? \what do you want to do?}
+
+// // mainTL.to("#hero",{duration:2, alpha:0});
+
+// mainTL.from("#hero >div", {
+//     duration: 1,
+//     alpha: 0,
+//     y: "-heroHight.clientHight",
+//     scale: 3
+// });
+// mainTL.from("#hero h1", {
+//     duration: 0.9,
+//     alpha: 0,
+//     y: "+=200"
+// },"-=50%");
