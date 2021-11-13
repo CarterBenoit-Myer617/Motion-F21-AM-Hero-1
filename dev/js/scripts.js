dd713735a5196125bfc3d875b33fc3c67f949d24
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2021-11-13 17:19:00 -0500

    up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,6 +6,9 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
+// gsap.set("#stem",{TransformOrigin:"center bottom"});
+gsap.set("#Plant-Clip",{TransformOrigin:"bottom"});
+
 
 const mainTl = gsap. timeline();
 
@@ -13,7 +16,11 @@ function GrowPlantOne(){
 
   const tl = gsap.timeline();
 
-  tl.from("#stem", {duration:0.5, scaleY: 0});
+  tl.from("#stem", {duration:1, scaleY:0});
+
+  tl.from("#Plant-Clip", {duration:1, scaleY:0});
+
+  // tl.from("#stem", {duration:0.5, drawSVG:"0%"});
 
   return tl;
 
